245c25cef48cfcfef7c558332af7632f36a863b1
Catch22 <github@kudige.com>
2026-08-01 01:51:53 +1000

    Complete public attachment grant contract

--- include/cubicle/attachment.h
+++ include/cubicle/attachment.h
@@ -35,13 +35,20 @@ typedef struct cubicle_attachment_request {
     uint64_t tty_offset;
     unsigned int rows;
     unsigned int cols;
+    cubicle_request_options_t request;
 } cubicle_attachment_request_t;
 
 typedef struct cubicle_attachment_grant {
+    cubicle_grant_id_t grant_id;
+    cubicle_manager_id_t manager_id;
+    cubicle_workspace_id_t workspace_id;
+    cubicle_process_id_t process_id;
+    cubicle_key_id_t client_key_id;
     cubicle_endpoint_t endpoint;
     char token[CUBICLE_TOKEN_MAX];
     uint64_t issued_at_ms;
     uint64_t expires_at_ms;
+    uint32_t connection_limit;
     cubicle_channel_mask_t granted_channels;
     cubicle_attachment_mode_t mode;
 } cubicle_attachment_grant_t;
@@ -51,39 +58,14 @@ typedef struct cubicle_attachment_options {
     int io_timeout_ms;
 } cubicle_attachment_options_t;
 
-cubicle_error_code_t cubicle_attachment_request(
-    cubicle_client_t *client,
-    const cubicle_attachment_request_t *request,
-    cubicle_attachment_grant_t *grant_out);
-
-cubicle_error_code_t cubicle_attachment_connect(
-    const cubicle_attachment_grant_t *grant,
-    const cubicle_attachment_options_t *options,
-    cubicle_attachment_t **attachment_out);
-
-ssize_t cubicle_attachment_read(
-    cubicle_attachment_t *attachment,
-    void *buffer,
-    size_t length);
-
-ssize_t cubicle_attachment_write(
-    cubicle_attachment_t *attachment,
-    const void *buffer,
-    size_t length);
-
-cubicle_error_code_t cubicle_attachment_resize(
-    cubicle_attachment_t *attachment,
-    unsigned int rows,
-    unsigned int cols);
-
-cubicle_error_code_t cubicle_attachment_close_input(
-    cubicle_attachment_t *attachment);
-
-const cubicle_error_t *cubicle_attachment_last_error(
-    const cubicle_attachment_t *attachment);
-
-void cubicle_attachment_disconnect(
-    cubicle_attachment_t *attachment);
+cubicle_error_code_t cubicle_attachment_request(cubicle_client_t *client, const cubicle_attachment_request_t *request, cubicle_attachment_grant_t *grant_out);
+cubicle_error_code_t cubicle_attachment_connect(const cubicle_attachment_grant_t *grant, const cubicle_attachment_options_t *options, cubicle_attachment_t **attachment_out);
+ssize_t cubicle_attachment_read(cubicle_attachment_t *attachment, void *buffer, size_t length);
+ssize_t cubicle_attachment_write(cubicle_attachment_t *attachment, const void *buffer, size_t length);
+cubicle_error_code_t cubicle_attachment_resize(cubicle_attachment_t *attachment, unsigned int rows, unsigned int cols);
+cubicle_error_code_t cubicle_attachment_close_input(cubicle_attachment_t *attachment);
+const cubicle_error_t *cubicle_attachment_last_error(const cubicle_attachment_t *attachment);
+void cubicle_attachment_disconnect(cubicle_attachment_t *attachment);
 
 #ifdef __cplusplus
 }
