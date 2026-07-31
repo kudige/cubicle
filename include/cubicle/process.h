60976d05ff6d7f29e4c9af616b33886c53901632
Catch22 <github@kudige.com>
2026-08-01 01:41:24 +1000

    Expand libcubicle process API

--- include/cubicle/process.h
+++ include/cubicle/process.h
@@ -1,11 +1,14 @@
 #ifndef CUBICLE_PROCESS_H
 #define CUBICLE_PROCESS_H
 
-#include <stdint.h>
+#include "cubicle/client_error.h"
+#include "cubicle/types.h"
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+typedef struct cubicle_client cubicle_client_t;
 
 typedef enum cubicle_process_mode {
     CUBICLE_PROCESS_STREAM = 0,
@@ -14,21 +17,135 @@ typedef enum cubicle_process_mode {
 } cubicle_process_mode_t;
 
 typedef enum cubicle_process_state {
-    CUBICLE_PROCESS_STARTING = 0,
-    CUBICLE_PROCESS_RUNNING = 1,
-    CUBICLE_PROCESS_EXITED = 2,
-    CUBICLE_PROCESS_FAILED = 3
+    CUBICLE_PROCESS_ALLOCATED = 0,
+    CUBICLE_PROCESS_STARTING,
+    CUBICLE_PROCESS_RUNNING,
+    CUBICLE_PROCESS_STOPPING,
+    CUBICLE_PROCESS_DRAINING,
+    CUBICLE_PROCESS_COMPLETED,
+    CUBICLE_PROCESS_FAILED,
+    CUBICLE_PROCESS_LOST,
+    CUBICLE_PROCESS_REMOVED
 } cubicle_process_state_t;
 
-typedef struct cubicle_process_ref {
-    char process_id[37];
-    char workspace_id[37];
-    char friendly_name[128];
+typedef enum cubicle_stdin_policy {
+    CUBICLE_STDIN_OPEN = 0,
+    CUBICLE_STDIN_EOF = 1
+} cubicle_stdin_policy_t;
+
+typedef enum cubicle_stream_kind {
+    CUBICLE_STREAM_STDOUT = 0,
+    CUBICLE_STREAM_STDERR,
+    CUBICLE_STREAM_TTY
+} cubicle_stream_kind_t;
+
+typedef struct cubicle_process_info {
+    cubicle_manager_id_t manager_id;
+    cubicle_workspace_id_t workspace_id;
+    cubicle_process_id_t id;
+    char friendly_name[CUBICLE_NAME_MAX];
     cubicle_process_mode_t mode;
     cubicle_process_state_t state;
-    int64_t pid;
-    int64_t process_group_id;
-} cubicle_process_ref_t;
+    int exit_code;
+    int termination_signal;
+    bool has_exit_status;
+    uint64_t stdout_offset;
+    uint64_t stderr_offset;
+    uint64_t tty_offset;
+    uint64_t created_at_ms;
+    uint64_t started_at_ms;
+    uint64_t exited_at_ms;
+    int64_t local_pid;
+    int64_t local_pgid;
+} cubicle_process_info_t;
+
+typedef struct cubicle_process_start_options {
+    const char *workspace_id;
+    const char *friendly_name;
+    cubicle_process_mode_t mode;
+    cubicle_stdin_policy_t stdin_policy;
+    const char *cwd;
+    const char *const *argv;
+    size_t argc;
+    const char *const *env;
+    size_t env_count;
+    unsigned int tty_rows;
+    unsigned int tty_cols;
+    cubicle_request_options_t request;
+} cubicle_process_start_options_t;
+
+typedef struct cubicle_process_filter {
+    const char *workspace_id;
+    const char *name_prefix;
+    const cubicle_process_state_t *states;
+    size_t state_count;
+    bool include_completed;
+} cubicle_process_filter_t;
+
+typedef struct cubicle_process_terminate_options {
+    int grace_period_ms;
+    bool force_after_grace;
+} cubicle_process_terminate_options_t;
+
+typedef struct cubicle_output_chunk {
+    uint64_t start_offset;
+    uint64_t next_offset;
+    bool end_of_stream;
+    void *data;
+    size_t length;
+} cubicle_output_chunk_t;
+
+cubicle_error_code_t cubicle_process_start(
+    cubicle_client_t *client,
+    const cubicle_process_start_options_t *options,
+    cubicle_process_info_t *process_out);
+
+cubicle_error_code_t cubicle_process_get(
+    cubicle_client_t *client,
+    const char *process_id_or_name,
+    const char *workspace_id,
+    cubicle_process_info_t *process_out);
+
+cubicle_error_code_t cubicle_process_list(
+    cubicle_client_t *client,
+    const cubicle_process_filter_t *filter,
+    cubicle_process_info_t **processes_out,
+    size_t *count_out);
+
+cubicle_error_code_t cubicle_process_signal(
+    cubicle_client_t *client,
+    const char *process_id,
+    int signal_number);
+
+cubicle_error_code_t cubicle_process_terminate(
+    cubicle_client_t *client,
+    const char *process_id,
+    const cubicle_process_terminate_options_t *options);
+
+cubicle_error_code_t cubicle_process_kill(
+    cubicle_client_t *client,
+    const char *process_id);
+
+cubicle_error_code_t cubicle_process_wait(
+    cubicle_client_t *client,
+    const char *process_id,
+    int timeout_ms,
+    cubicle_process_info_t *process_out);
+
+cubicle_error_code_t cubicle_process_remove(
+    cubicle_client_t *client,
+    const char *process_id);
+
+cubicle_error_code_t cubicle_process_read_output(
+    cubicle_client_t *client,
+    const char *process_id,
+    cubicle_stream_kind_t stream,
+    uint64_t offset,
+    size_t maximum_length,
+    cubicle_output_chunk_t *chunk_out);
+
+void cubicle_process_list_free(cubicle_process_info_t *processes);
+void cubicle_output_chunk_free(cubicle_output_chunk_t *chunk);
 
 const char *cubicle_process_mode_name(cubicle_process_mode_t mode);
 const char *cubicle_process_state_name(cubicle_process_state_t state);
