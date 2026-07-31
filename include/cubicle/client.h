0bd7d2ef2bdb3acdf67084fa68b0099f0d2d32de
Catch22 <github@kudige.com>
2026-08-01 01:43:19 +1000

    Refocus libcubicle client connection API

--- include/cubicle/client.h
+++ include/cubicle/client.h
@@ -1,11 +1,14 @@
 #ifndef CUBICLE_CLIENT_H
 #define CUBICLE_CLIENT_H
 
+#include "cubicle/auth.h"
 #include "cubicle/client_error.h"
-#include "cubicle/client_types.h"
+#include "cubicle/types.h"
 #include "cubicle/transport.h"
 
-#include <stddef.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
 
 typedef struct cubicle_client cubicle_client_t;
 
@@ -14,6 +17,7 @@ typedef struct cubicle_client_options {
     int connect_timeout_ms;
     int request_timeout_ms;
     cubicle_transport_t *transport;
+    cubicle_auth_options_t auth;
 } cubicle_client_options_t;
 
 cubicle_error_code_t cubicle_client_connect(
@@ -25,29 +29,12 @@ void cubicle_client_disconnect(cubicle_client_t *client);
 const cubicle_error_t *cubicle_client_last_error(
     const cubicle_client_t *client);
 
-cubicle_error_code_t cubicle_manager_ping(cubicle_client_t *client);
+cubicle_error_code_t cubicle_client_session_info(
+    const cubicle_client_t *client,
+    cubicle_session_info_t *session_out);
 
-cubicle_error_code_t cubicle_workspace_create(
-    cubicle_client_t *client,
-    const char *name,
-    cubicle_workspace_info_t *workspace_out);
-
-cubicle_error_code_t cubicle_workspace_list(
-    cubicle_client_t *client,
-    cubicle_workspace_info_t **workspaces_out,
-    size_t *count_out);
-
-cubicle_error_code_t cubicle_process_start(
-    cubicle_client_t *client,
-    const cubicle_process_start_options_t *options,
-    cubicle_process_info_t *process_out);
-
-cubicle_error_code_t cubicle_process_get(
-    cubicle_client_t *client,
-    const char *process_id_or_name,
-    const char *workspace_id,
-    cubicle_process_info_t *process_out);
-
-void cubicle_workspace_list_free(cubicle_workspace_info_t *workspaces);
+#ifdef __cplusplus
+}
+#endif
 
 #endif
