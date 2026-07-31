b72799399289e4ae98727f7200f6d0d56bff0f27
Catch22 <github@kudige.com>
2026-08-01 01:44:20 +1000

    Add libcubicle public header compile test

---
+++ tests/public_headers_test.c
@@ -0,0 +1,18 @@
+#include "cubicle/cubicle.h"
+#include "cubicle/transport_unix.h"
+
+int main(void)
+{
+    cubicle_workspace_info_t workspace = {0};
+    cubicle_process_info_t process = {0};
+    cubicle_attachment_request_t attachment = {0};
+    cubicle_event_query_t events = {0};
+    cubicle_manager_status_t manager = {0};
+
+    (void)workspace;
+    (void)process;
+    (void)attachment;
+    (void)events;
+    (void)manager;
+    return 0;
+}
